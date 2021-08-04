4cda83c68af3618643cda90eb1fc6138f48ecce6
alphaqu <24830855+alphaqu@users.noreply.github.com>
2021-08-04 10:26:49 +0200

    String Helper

--- src/main/java/net/oskarstrom/dashloader/core/util/StringHelper.java
+++ src/main/java/net/oskarstrom/dashloader/core/util/StringHelper.java
@@ -8,10 +8,8 @@ public class StringHelper {
 	private static final Pattern REGEX = Pattern.compile("\\{}");
 
 	public static String of(String str, Object... parameters) {
-		final Iterator<Object> iterator = Arrays.stream(parameters).iterator();
-		return REGEX.matcher(str).replaceAll(matchResult -> {
-			return iterator.hasNext() ? iterator.next().toString() : matchResult.group();
-		});
+		final Iterator<String> iterator = Arrays.stream(parameters).map(Object::toString).iterator();
+		return REGEX.matcher(str).replaceAll(matchResult -> iterator.hasNext() ? iterator.next() : matchResult.group());
 	}
 
 }
