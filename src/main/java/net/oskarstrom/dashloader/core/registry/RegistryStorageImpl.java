64e0a8cff10954a3dd448c531361d5d25a2ef426
alphaqu <24830855+alphaqu@users.noreply.github.com>
2021-08-05 12:49:01 +0200

    Arrayed RegistryStorage

--- src/main/java/net/oskarstrom/dashloader/core/registry/RegistryStorageImpl.java
+++ src/main/java/net/oskarstrom/dashloader/core/registry/RegistryStorageImpl.java
@@ -8,52 +8,73 @@ import net.oskarstrom.dashloader.api.registry.DashRegistry;
 import net.oskarstrom.dashloader.api.registry.FactoryConstructor;
 import net.oskarstrom.dashloader.api.registry.RegistryStorage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class RegistryStorageImpl<F, D extends Dashable<F>> implements RegistryStorage<F>, Dashable<List<F>> {
-	private final List<F> unDashedObjects = new ArrayList<>();
 	private final Object2IntMap<F> deduplicationMap = new Object2IntOpenHashMap<>();
 	private final DashRegistry registry;
-	private final List<D> dashables;
+	private final F[] unDashedObjects; // bound to F
+	private D[] dashables; // bound to D
+	private int pos = 0;
 
 	public RegistryStorageImpl(DashRegistry registry) {
 		this.registry = registry;
-		this.dashables = new ArrayList<>();
+		this.unDashedObjects = null;
+		//noinspection unchecked
+		this.dashables = (D[]) new Dashable[1];
 	}
 
-	public RegistryStorageImpl(List<D> dashables, DashRegistry registry) {
+	public RegistryStorageImpl(DashRegistry registry, D[] dashables) {
 		this.registry = registry;
 		this.dashables = dashables;
+		//noinspection unchecked
+		this.unDashedObjects = (F[]) new Object[dashables.length];
 	}
 
 	@Override
 	public int add(F object) {
 		if (deduplicationMap.containsKey(object))
 			return deduplicationMap.getInt(object);
-
 		final D dashObject = create(object, registry);
-		final int pos = dashables.size();
-		dashables.add(dashObject);
+
+		dashables = ensureSize(dashables.length + 1, dashables);
+		int pos = this.pos;
+		dashables[pos] = dashObject;
 		deduplicationMap.put(object, pos);
+		this.pos++;
 		return pos;
 	}
+
+	private D[] ensureSize(int size, D[] array) {
+		if (array.length < size) {
+			//noinspection unchecked
+			D[] newArray = (D[]) new Dashable[array.length * 2];
+			System.arraycopy(array, 0, newArray, 0, array.length);
+			return newArray;
+		}
+		return array;
+	}
+
 
 	public abstract D create(F object, DashRegistry registry);
 
 
-	public List<D> getDashables() {
-		return dashables;
+	@SuppressWarnings("unchecked")
+	public D[] getDashables() {
+		D[] trimmedArray = (D[]) new Dashable[pos + 1];
+		if (pos >= 0) System.arraycopy(dashables, 0, trimmedArray, 0, pos + 1);
+		return trimmedArray;
 	}
 
 	@Override
 	public F get(int pointer) {
-		return unDashedObjects.get(pointer);
+		//noinspection ConstantConditions
+		return unDashedObjects[pointer];
 	}
 
 	@Override
 	public List<F> toUndash(DashRegistry registry) {
-		if (dashables == null || dashables.isEmpty()) {
+		if (dashables == null || dashables.length == 0) {
 			throw new IllegalStateException("Dashables are not available.");
 		}
 		return null;
