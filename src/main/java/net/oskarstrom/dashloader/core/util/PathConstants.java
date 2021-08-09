51691289324cf9d142d34444b32cf4d515bf4ec4
leoc200 <22406910+leocth@users.noreply.github.com>
2021-08-09 19:44:02 +0800

    javadoc doesnt like that

--- src/main/java/net/oskarstrom/dashloader/core/util/PathConstants.java
+++ src/main/java/net/oskarstrom/dashloader/core/util/PathConstants.java
@@ -2,12 +2,12 @@ package net.oskarstrom.dashloader.core.util;
 
 public final class PathConstants {
     /**
-     * <h2>.dlc / DashLoader Cache File extension.</h2>
+     * .dlc / DashLoader Cache File extension.
      * Used for caching internal systems like the serializers.
      */
     public static final String CACHE_EXTENSION = ".dlc";
     /**
-     * <h2>.dld / DashLoader Data File extension.</h2>
+     * .dld / DashLoader Data File extension.
      * Used for the cache data that is created by the serializers.
      */
     public static final String DATA_EXTENSION = ".dld";
