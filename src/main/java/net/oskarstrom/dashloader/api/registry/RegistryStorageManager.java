139483aa0d6381302f7c45f027c82ce51fbda2e6
alphaqu <24830855+alphaqu@users.noreply.github.com>
2021-08-14 11:56:30 +0200

    Lol forgot interface

--- src/main/java/net/oskarstrom/dashloader/api/registry/RegistryStorageManager.java
+++ src/main/java/net/oskarstrom/dashloader/api/registry/RegistryStorageManager.java
@@ -6,6 +6,7 @@ import net.oskarstrom.dashloader.api.Dashable;
 public interface RegistryStorageManager {
 	<F, D extends Dashable<F>> RegistryStorage<F> createSimpleRegistry(Class<F> rawClass, Class<D> dashClass, DashRegistry registry);
 
-	<F, D extends Dashable<F>> RegistryStorage<F> createMultiRegistry(Object2ObjectMap<Class<F>, Class<D>> classes, DashRegistry registry);
+	<F, D extends Dashable<F>> RegistryStorage<F> createSupplierRegistry(D[] data, DashRegistry registry);
 
+	<F, D extends Dashable<F>> RegistryStorage<F> createMultiRegistry(Object2ObjectMap<Class<F>, Class<D>> classes, DashRegistry registry);
 }
